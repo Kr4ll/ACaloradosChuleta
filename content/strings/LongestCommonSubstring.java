2fe4f02a5a5953315cdfef4b4157fad6cba1c8ad
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 20:48:20 +0000

    Update LongestCommonSubstring.java

--- content/strings/LongestCommonSubstring.java
+++ content/strings/LongestCommonSubstring.java
@@ -1,31 +1,32 @@
-    // Function to find the length of the longest LCS
-    static int LCSubStr(String s,String t,
-                        int n,int m) 
-    {  
-        // Create DP table
-        int dp[][]=new int[2][m+1];
-        int res=0;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s.charAt(i-1)==t.charAt(j-1))
-                {
-                    dp[i%2][j]=dp[(i-1)%2][j-1]+1;
-                    if(dp[i%2][j]>res)
-                        res=dp[i%2][j];
-                }
-                else dp[i%2][j]=0;
-            }
-        }
-        return res;
-    }
-    public static void main (String[] args)
+// Function to find the length of the longest common string
+// between two strings
+static int LCSubStr(String s,String t,
+                    int n,int m) 
+{  
+    // Create DP table
+    int dp[][]=new int[2][m+1];
+    int res=0;
+    for(int i=1;i<=n;i++)
     {
-        String X="OldSite:GeeksforGeeks.org"; 
-        String Y="NewSite:GeeksQuiz.com";
-        int m=X.length();
-        int n=Y.length();
-        System.out.println(LCSubStr(X,Y,m,n));
-        // Output: Geeks
+        for(int j=1;j<=m;j++)
+        {
+            if(s.charAt(i-1)==t.charAt(j-1))
+            {
+                dp[i%2][j]=dp[(i-1)%2][j-1]+1;
+                if(dp[i%2][j]>res)
+                    res=dp[i%2][j];
+            }
+            else dp[i%2][j]=0;
+        }
     }
+    return res;
+}
+public static void main (String[] args)
+{
+    String X="OldSite:GeeksforGeeks.org"; 
+    String Y="NewSite:GeeksQuiz.com";
+    int m=X.length();
+    int n=Y.length();
+    System.out.println(LCSubStr(X,Y,m,n));
+    // Output: Geeks
+}
