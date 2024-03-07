0b6cd8ea9b043655432bd07d983fbac26d5475b3
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 22:58:26 +0000

    Create UnorderedPriorityQueue.py

---
+++ content/data-structures/UnorderedPriorityQueue.py
@@ -0,0 +1,8 @@
+# importing "heapq" to implement heap queue
+import heapq
+# initializing list
+li = [5, 7, 9, 1, 3]
+# using heapify to convert list into heap
+heapq.heapify(li)
+# printing created heap
+print ("The created heap is : ",(list(li)))
