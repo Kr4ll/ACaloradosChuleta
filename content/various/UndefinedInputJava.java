661036524960e2c64af2af0cf880819ce200446e
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 23:38:02 +0000

    Update UndefinedInputJava.java

--- content/various/UndefinedInputJava.java
+++ content/various/UndefinedInputJava.java
@@ -1,28 +1,20 @@
-/**
- * Author: Alberto Dekeno
- * License: CC0
- * Source: Codeforces
- * Description: Para numero de entradas indeterminadas 
- * Time: no time
- * Status: no tested
- */
+// Cuando no hay un input definido y ademas no queremos perder lo consumido
+boolean hayEntrada = true;
+while (hayEntrada) {
+    Map.Entry<Boolean, Optional<String>> hasNextObject = fastReader.hasNext();
+    hayEntrada = hasNextObject.getKey();
+    if (hayEntrada) {
+	//Haciendo hasNextObject.getValue() obtenemos el valor que consumio el hasNext
+	List<Integer> list = Arrays.stream(hasNextObject.getValue().get().split(" "))
+		.map(Integer::parseInt).collect(Collectors.toList());
+    }
+}
+fastReader.close();
 
- 	boolean hayEntrada = true;
-        while (hayEntrada) {
-            Map.Entry<Boolean, Optional<String>> hasNextObject = fastReader.hasNext();
-            hayEntrada = hasNextObject.getKey();
-            if (hayEntrada) {
-		//Haciendo hasNextObject.getValue() obtenemos el valor que consumio el hasNext
-                List<Integer> list = Arrays.stream(hasNextObject.getValue().get().split(" "))
-                        .map(Integer::parseInt).collect(Collectors.toList());
-            }
-        }
-        fastReader.close();
-
-        while (fastReader.hasNext().getKey()) {
-                //En este caso lo consumido por hasNext no nos interesa
-                List<Integer> list = Arrays.stream(fastReader.nextLine().split(" "))
-                        .map(Integer::parseInt).collect(Collectors.toList());
-        }
-        fastReader.close();
+// Cuando no hay un input definido y lo consumido no nos interesa
+while (fastReader.hasNext().getKey()) {
+	List<Integer> list = Arrays.stream(fastReader.nextLine().split(" "))
+		.map(Integer::parseInt).collect(Collectors.toList());
+}
+fastReader.close();
 
