3356bec1336f3270c6131cd1c43a789584b37adf
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 19:33:18 +0000

    Update and rename KitchenKit.h to TricksC++.h

--- content/various/TricksC++.h
+++ content/various/TricksC++.h
@@ -1,17 +1,7 @@
-/**
- * Author: Alberto Dekeno
- * License: CC0
- * Source: Codeforces
- * Description: Tricks for the contest
- * Time: no time
- * Status: tested on http://codeforces.com/contest/321/problem/E
- */
 #pragma once
-
 line of vectors c++:
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef vector<int> vi;
 typedef pair<int,int> pii;
 typedef vector<pii> vii;
@@ -20,9 +10,6 @@ typedef map<string, int> msi;
 	 getline(std::cin, line);
 	istringstream iss(line);
 	int num;
-	
 	while (iss >> num) {
        		 numbers.push_back(num);
    	 }
-   	 
-  
