57f7f330ec3b677374fc49388eb89a1b8fd05257
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 14:22:42 +0000

    Update EulerTotientFunction.py

--- content/math/EulerTotientFunction.py
+++ content/math/EulerTotientFunction.py
@@ -2,7 +2,6 @@
 # Return the count of numbers in {1, 2, 3, …, n-1} that are relatively prime to n,
 # i.e., the numbers whose GCD (Greatest Common Divisor) with n is 1.
 def phi(n):
- 
     result = 1
     for i in range(2, n):
         if (gcd(i, n) == 1):
