7ad3b8a2c3904f1da8f39460f255ba9a8bd613c1
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 14:23:42 +0000

    Update FactorialWithModule.py

--- content/math/FactorialWithModule.py
+++ content/math/FactorialWithModule.py
@@ -1,9 +1,7 @@
 def factorial(n) :
     M = 1000000007
     f = 1
- 
     for i in range(1, n + 1): 
-        f = (f * i) % M # Now f never can 
-                        # exceed 10^9+7 
- 
+        # Now f never can exceed 10^9+7
+        f = (f * i) % M
     return f 
