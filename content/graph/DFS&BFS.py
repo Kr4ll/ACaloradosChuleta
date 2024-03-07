3be0102b60da1000ee2489662f69abf8c24ae210
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 22:09:44 +0000

    Update and rename dfs-bfs.py to DFS&BFS.py

--- content/graph/DFS&BFS.py
+++ content/graph/DFS&BFS.py
@@ -1,5 +1,5 @@
 from collections import deque
-
+# DFS
 def dfs(g, visited, v):
 	q = deque()
 	q.append(v)
@@ -10,21 +10,19 @@ def dfs(g, visited, v):
 		for adj in reversed(g[aux]):
 			if not visited[adj]:
 				q.append(adj)
-#Recursivo
+# DFS Recursivo
 def dfsRec(g, visited, v):      
     visited[v] = True
     for adj in g[v]:
         if not visited[adj]:
             dfsRec(g,visited,adj)
-
 def dfs(g):
     n = len(g) 
     visited = [False]*n  
     for v in range(1,n):   
         if not visited[v]:
-            dfsRec(g,visited,v) 
-            
-            
+            dfsRec(g,visited,v)
+# BFS
 def bfs(g, visited, v):
 	q = deque()
 	q.append(v)
@@ -36,10 +34,5 @@ def bfs(g, visited, v):
 		for adj in g[aux]:
 			if not visited[adj]:
 				q.append(adj)
-				
-Ordenar listas con dos keys:minus means decreasing way
+# Ordenar listas con dos keys:minus means decreasing way
 s = sorted(s, key = lambda x: (-x[0], x[1], x[2]))
-		
-
-
-
