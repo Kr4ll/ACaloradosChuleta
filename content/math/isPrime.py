3952469d699a48676d18007d93893ec4efa7b88a
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 13:31:35 +0000

    Create isPrime.py

---
+++ content/math/isPrime.py
@@ -0,0 +1,12 @@
+def isPrime(n): 
+  
+    # Corner case 
+    if n <= 1: 
+        return False
+  
+    # Check from 2 to n-1 
+    for i in range(2, n): 
+        if n % i == 0: 
+            return False
+  
+    return True
