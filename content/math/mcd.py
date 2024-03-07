2e9e04c1affa8eebdd9c74ae9b8142ab4ddd4b09
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-07 13:45:50 +0000

    Update mcd.py

--- content/math/mcd.py
+++ content/math/mcd.py
@@ -1,2 +1,14 @@
-# inbuilt maximo comun divisor function gcd() using math library
-mcd = math.gcd(a, b) 
+# Python program to demonstrate working of extended
+# Euclidean Algorithm
+# Alternativa usar el inbuilt de math.gcd()
+# function for extended Euclidean Algorithm
+def gcdExtended(a, b):
+    # Base Case
+    if a == 0:
+        return b, 0, 1
+    gcd, x1, y1 = gcdExtended(b % a, a)
+    # Update x and y using results of recursive
+    # call
+    x = y1 - (b//a) * x1
+    y = x1
+    return gcd, x, y
