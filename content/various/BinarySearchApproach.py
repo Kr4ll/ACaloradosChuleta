e5717ebfc66029c1b4d769295ed161bb4a6da05e
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2024-03-08 07:34:56 +0000

    Create BinarySearchApproach.py

---
+++ content/various/BinarySearchApproach.py
@@ -0,0 +1,17 @@
+lower_bound = 2
+upper_bound = 10
+# Number to be guessed is 9
+# Applying Binary Search interactively
+while (lower_bound <= upper_bound) :
+    mid = (lower_bound + upper_bound) // 2
+    # Print guessed number
+    print(mid)
+    # Input the response from the judge
+    response = int(input())
+    if (response == -1) :
+        lower_bound = mid + 1
+    elif (response == 1) :
+        upper_bound = mid - 1
+    elif (response == 0) :
+        print("Number guessed is :", mid)
+        break
